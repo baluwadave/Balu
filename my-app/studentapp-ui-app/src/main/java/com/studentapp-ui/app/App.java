317f3bf99a9d6f5aa1a67a6a78a7feffb4d553a8
root <root@ip-172-31-46-222.ap-south-1.compute.internal>
2024-04-08 09:54:04 +0000

    java project build

---
+++ my-app/studentapp-ui-app/src/main/java/com/studentapp-ui/app/App.java
@@ -0,0 +1,13 @@
+package com.studentapp-ui.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
