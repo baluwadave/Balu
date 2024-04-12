54dd5d0a36681e010a5d5e6836907f33291a3c9f
Balu <baluwadave@gmail.com>
2024-04-12 15:51:13 +0530

    --- Auto Git Commit ---

--- studentapp_ui.groovy
+++ studentapp_ui.groovy
@@ -21,6 +21,7 @@ pipeline {
                 -Dsonar.host.url=http://13.127.230.27:9000 \
                 -Dsonar.login=0eb275c507d99873e3263734c5e2f7a3bb805db3                
                 echo "test done"
+                waitForQualityGate abortPipeline: false, credentialsId: 'sonar-token'
             }
         } 
         stage ('Quality-Test'){
