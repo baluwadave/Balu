00057ad2551ed03d9efc8cd7b0b47575f377877d
Balu <baluwadave@gmail.com>
2024-04-12 17:54:43 +0530

    --- Auto Git Commit ---

--- studentapp_ui.groovy
+++ studentapp_ui.groovy
@@ -17,10 +17,12 @@ pipeline {
         }
         stage ('Test'){
             steps{
+
                 sh '''/opt/apache-maven-3.9.6/bin/mvn sonar:sonar \\
                 -Dsonar.projectKey=studentapp-ui \\
                 -Dsonar.host.url=http://13.127.46.220:9000 \\
-                -Dsonar.login=3beda3d515009d7f90566b92bd884563ad2657cf'''           
+                -Dsonar.login=3beda3d515009d7f90566b92bd884563ad2657cf
+                '''                       
                 echo "test done"
                 waitForQualityGate abortPipeline: false, credentialsId: 'sonar-token'
             }
