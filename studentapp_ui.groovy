92b5f1c8fa565e06fbfa8229d2f09fc2d040160e
Balu <baluwadave@gmail.com>
2024-04-12 14:40:14 +0530

    --- Auto Git Commit ---

--- studentapp_ui.groovy
+++ studentapp_ui.groovy
@@ -16,6 +16,10 @@ pipeline {
         }
         stage ('Test'){
             steps{
+                mvn sonar:sonar \
+                -Dsonar.projectKey=studentapp-ui \
+                -Dsonar.host.url=http://13.127.230.27:9000 \
+                -Dsonar.login=0eb275c507d99873e3263734c5e2f7a3bb805db3                
                 echo "test done"
             }
         } 
