89681e995aa82930be69f8ec1cbc856ea5dc1aa1
Balu <baluwadave@gmail.com>
2024-04-13 14:17:51 +0530

    --- Auto Git Commit ---

--- studentapp_ui.groovy
+++ studentapp_ui.groovy
@@ -17,7 +17,7 @@ pipeline {
         }
         stage ('Test'){
             steps{
-                withSonarQubeEnv(credentialsId: 'sonar-token',installationName: 'sonar') {
+                withSonarQubeEnv(credentialsId: 'sonar-token',installationName: 'sonarqube') {
                     sh '''/opt/apache-maven-3.9.6/bin/mvn sonar:sonar \\
                     -Dsonar.projectKey=studentapp-ui \\
                     -Dsonar.host.url=http://13.232.148.171:9000 \\
